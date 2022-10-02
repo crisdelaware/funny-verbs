6a5a7055bba2b4748a6b616a65f081f1657efa24
crisdelaware <crisdelaware@gmail.com>
2022-10-02 18:22:24 -0500

    Avance proyecto

---
+++ verbs.js
@@ -0,0 +1,24 @@
+
+
+const verbs = [
+    'run',
+    'jump',
+    'swim',
+    'hit',
+    'eat',
+    'sing',
+    'walk',
+    'drink',
+    'wear'
+];
+
+const verbos = [
+    'correr',
+    'saltar',
+    'nadar',
+    'comer',
+    'cantar',
+    'caminar',
+    'beber,',
+    'usar'
+];
